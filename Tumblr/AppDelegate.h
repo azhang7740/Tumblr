3e6ab2a2cdc5f94bc2cf1804a25e196caeba3801
Angelina Zhang <angelinaz@fb.com>
2022-06-16 09:51:08 -0700

    Initial Commit

---
+++ Tumblr/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  Tumblr
+//
+//  Created by Angelina Zhang on 6/16/22.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ Tumblr/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  Tumblr
+//
+//  Created by Angelina Zhang on 6/16/22.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
